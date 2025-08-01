5945605aa3c02d8c6c59d755f5c37857f17ab867
bamshak9 <b.fwangyil@alustudent.com>
2025-08-01 13:08:46 +0100

    added double_dict

---
+++ double_dict.py
@@ -0,0 +1,10 @@
+student_1 = {
+        "Name": "Tom",
+        "University": "UJ",
+        "Department": "CS"
+}
+student_2 = student_1.copy()
+student_2["Name"] = "Kevin"
+print("Student 1:", student_1)
+print("Student 2:", student_2)
+
