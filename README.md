197ca21f63ac7974335df8a6b1f621dc4a514c29
bamshak9 <b.fwangyil@alustudent.com>
2025-08-01 12:04:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains the work done in blockfuse labs class on 1st August 2025.
---
+++ student.py
@@ -0,0 +1,24 @@
+student = {
+        "name": "Tom",
+        "department":"Computer Science",
+        "grades":[{"csc 111": 90, "Maths 101": 50, "physics": 70}],
+        "address": {"city": "Jos", "town": "Rayfield", "house no.": "PL/ray/00001"},
+        "fav_colors": ["Pink", "Red", "Black", "White"]
+}
+#Accessing dictionary items
+print(student["name"])
+print(student["department"])
+print(f"CSC 111 Score:", student["grades"][0]["csc 111"])
+print("Favorite color:", student["fav_colors"][2])
+#Dictionary Methods
+print(student.get("name"))
+print(student.keys())
+print(student.items())
+
+print("-----------------Updating Student Data--------------------")
+student["name"] = "Jerry"
+print(student)
+student.pop("fav_colors")
+print("------------------After popping-------------------")
+print(student)
+
